de8ee19b76e9270a2ba7522503d36fdd67b70a87
nemomalila <140974435+nemomalila@users.noreply.github.com>
2023-12-29 22:23:44 +0200

    Add files via upload

---
+++ aq68lam52v10/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean' -- if that doesn't work, try 'adamant' or 'bodacious'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq68lam52v10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq68lam52v10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq68lam52v10'
